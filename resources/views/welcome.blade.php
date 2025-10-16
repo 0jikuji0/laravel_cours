a9635fd38ac787b468756bf6b53a66e614df3f99
0jikuji0 <k13joj@proton.me>
2025-10-16 12:54:28 +0200

    intro alpinejs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,18 @@
 <x-layouts.home>
     <h2>Bienvenue sur le site de l'esgi en laravel</h2>
+
+    <div 
+    class="w-full shadow rounded flex flex-col gap-4 bg-white mt-8"  
+    x-data="{
+        open: false,
+    }">
+        <button 
+        class="btn btn-primary"
+        x-on:click="open = !=open"
+        >
+        Toggle content</button>
+        <div x-show="open">
+            My content
+        </div>
+    </div>
 </x-layouts.home>
