ac4e7bd8dc95a44408e58974ba7c416835c4e21d
0jikuji0 <k13joj@proton.me>
2025-10-30 08:52:26 +0100

    add collapse with open animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,21 @@
     <h2>Bienvenue sur le site de l'esgi en laravel</h2>
 
     <div 
-    class="w-full shadow rounded flex flex-col gap-4 bg-white mt-8"  
+    class="w-full shadow rounded flex flex-col bg-white mt-8"  
     x-data="{
         open: false,
+        openDropdown(){
+            this.open = !this.open;
+        }
     }">
         <button 
             class="btn btn-primary"
-            x-on:click="open = !open"
+            x-on:click="openDropdown"
         >
         Toggle content
         </button>
-        <div x-show="open">
-            My content
+        <div x-show="open" x-collapse>
+            <div class="px-4 py-2">My content</div> <!-- open animation with alpinejs use collapse-->
         </div>
     </div>
 </x-layouts.home>
